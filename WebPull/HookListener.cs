5ad4e27e993898b80de69ad00cd245b3b2b5facd
Marcel Walk <walk.marcel.97@googlemail.com>
2020-09-23 16:24:04 +0200

    Added output messages

---
+++ WebPull/HookListener.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WebPull
+{
+    class HookListener
+    {
+
+        TcpListener tcpListener;
+
+        public HookListener()
+        {
+            tcpListener = new TcpListener(
+                          IPAddress.Parse(ConfigurationManager.AppSettings["LocalIp"]),
+                          int.Parse(ConfigurationManager.AppSettings["LocalPort"])
+                      );
+        }
+
+        public void Start()
+        {
+            tcpListener.Start();
+
+            while (true)
+            {
+                var client = tcpListener.AcceptTcpClient();
+                NetworkStream s = client.GetStream();
+                StringBuilder sb = new StringBuilder();
+
+                Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] Hook recieved");
+
+                while (s.DataAvailable)  //while the client is connected, we look for incoming messages
+                {
+                    byte[] msg = new byte[1024];     //the messages arrive as byte array
+                    s.Read(msg, 0, msg.Length);
+                    var dbg = Encoding.UTF8.GetString(msg).Trim('\0'); //the same networkstream reads the message sent by the client
+                    sb.Append(dbg); //now , we write the message as string
+                }
+
+                var debug = sb.ToString();
+                if (!string.IsNullOrEmpty(debug))
+                {
+                    GitData data = JsonConvert.DeserializeObject<GitData>(debug.Substring(debug.IndexOf("{")));
+
+                    if (Directory.Exists(ConfigurationManager.AppSettings["OutDir"]))
+                    {
+                        Directory.Delete(ConfigurationManager.AppSettings["OutDir"], true);
+                    }
+
+                    LibGit2Sharp.Repository.Clone(data.Repository.CloneUrl.ToString(), ConfigurationManager.AppSettings["OutDir"]);
+
+                    SendOkayResponse(s, "Ok");
+                    Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] Repo published!");
+                }
+                else
+                {
+                    Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] Execution failed");
+                }
+
+
+                client.Close();
+            }
+
+            static void SendOkayResponse(NetworkStream s, string content)
+            {
+                System.IO.StreamWriter writer = new System.IO.StreamWriter(s);
+                writer.Write("HTTP/1.0 200 OK");
+                writer.Write(Environment.NewLine);
+                writer.Write("Content-Type: text/plain; charset=UTF-8");
+                writer.Write(Environment.NewLine);
+                writer.Write("Content-Length: " + content.Length);
+                writer.Write(Environment.NewLine);
+                writer.Write(Environment.NewLine);
+                writer.Write(content);
+                writer.Flush();
+            }
+        }
+    }
+}
--- WebPull/Program.cs
+++ WebPull/Program.cs
@@ -14,56 +14,8 @@ namespace WebPull
     {
         static void Main(string[] args)
         {
-            TcpListener tcpListener = new TcpListener(
-                IPAddress.Parse(ConfigurationManager.AppSettings["LocalIp"]),
-                int.Parse(ConfigurationManager.AppSettings["LocalPort"])
-            );
-            tcpListener.Start();
-
-            while (true)
-            {
-                var client = tcpListener.AcceptTcpClient();
-                NetworkStream s = client.GetStream();
-                StringBuilder sb = new StringBuilder();
-
-                while (s.DataAvailable)  //while the client is connected, we look for incoming messages
-                {
-                    byte[] msg = new byte[1024];     //the messages arrive as byte array
-                    s.Read(msg, 0, msg.Length);
-                    var dbg = Encoding.UTF8.GetString(msg).Trim('\0'); //the same networkstream reads the message sent by the client
-                    sb.Append(dbg); //now , we write the message as string
-                }
-
-                var debug = sb.ToString();
-                if (!string.IsNullOrEmpty(debug))
-                {
-                    GitData data = JsonConvert.DeserializeObject<GitData>(debug.Substring(debug.IndexOf("{")));
-
-                    byte[] hello = new byte[100];
-                    hello = Encoding.Default.GetBytes("200");
-
-                    s.Write(hello, 0, hello.Length);
-
-                    if (Directory.Exists(ConfigurationManager.AppSettings["OutDir"]))
-                    {
-                        Directory.Delete(ConfigurationManager.AppSettings["OutDir"], true);
-                    }
-           
-                    LibGit2Sharp.Repository.Clone(data.Repository.CloneUrl.ToString(), ConfigurationManager.AppSettings["OutDir"]);
-                   
-
-                }
-                else
-                {
-                    byte[] hello = new byte[100];
-                    hello = Encoding.Default.GetBytes("404");
-                    s.Write(hello, 0, hello.Length);
-                }
-
-
-                client.Close();
-            }
-
+            HookListener hook = new HookListener();
+            hook.Start();
         }
     }
 }
