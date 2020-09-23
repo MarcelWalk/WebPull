2932f199e5a68858a9e77c0f109381a35fd19dc3
Marcel Walk <walk.marcel.97@googlemail.com>
2020-09-23 12:59:07 +0200

    Fixed situation where repo alreadz exists

--- WebPull/Program.cs
+++ WebPull/Program.cs
@@ -1,6 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using LibGit2Sharp;
+using Newtonsoft.Json;
 using System;
 using System.Configuration;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -29,24 +32,42 @@ namespace WebPull
                     s.Read(msg, 0, msg.Length);
                     var dbg = Encoding.UTF8.GetString(msg).Trim('\0'); //the same networkstream reads the message sent by the client
                     sb.Append(dbg); //now , we write the message as string
-                    Console.WriteLine(dbg);
                 }
 
                 var debug = sb.ToString();
                 if (!string.IsNullOrEmpty(debug))
                 {
                     GitData data = JsonConvert.DeserializeObject<GitData>(debug.Substring(debug.IndexOf("{")));
-                    
-                    byte[] hello = new byte[100];   
-                    hello = Encoding.Default.GetBytes("K, thx bye!");
+
+                    byte[] hello = new byte[100];
+                    hello = Encoding.Default.GetBytes("200");
 
                     s.Write(hello, 0, hello.Length);
 
-                    if(Directory.Exists(ConfigurationManager.AppSettings["OutDir"]))
-                        Directory.Delete(ConfigurationManager.AppSettings["OutDir"]);
-                        
-                    LibGit2Sharp.Repository.Clone(data.Repository.CloneUrl.ToString(), ConfigurationManager.AppSettings["OutDir"]);
+                    if (Directory.Exists(ConfigurationManager.AppSettings["OutDir"]))
+                    {
+                        using (var repo = new LibGit2Sharp.Repository(ConfigurationManager.AppSettings["OutDir"]))
+                        {
+                            string logMessage = "";
+                            var remote = repo.Network.Remotes["origin"];
+                            var refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
+                            Commands.Fetch(repo, remote.Name, refSpecs, null, logMessage);
+                        }
+                    }
+                    else
+                    {
+                        LibGit2Sharp.Repository.Clone(data.Repository.CloneUrl.ToString(), ConfigurationManager.AppSettings["OutDir"]);
+                    }
+
                 }
+                else
+                {
+                    byte[] hello = new byte[100];
+                    hello = Encoding.Default.GetBytes("404");
+
+                    s.Write(hello, 0, hello.Length);
+                }
+
 
                 client.Close();
             }
