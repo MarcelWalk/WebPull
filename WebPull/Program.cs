7c5b26341bf4f54b3ca54c37958305755ccc76a0
Marcel Walk <walk.marcel.97@googlemail.com>
2020-09-24 01:53:26 +0200

    Added Post Execution file

--- WebPull/Program.cs
+++ WebPull/Program.cs
@@ -16,8 +16,33 @@ namespace WebPull
         {
             while(true){
                 try{
-                    HookListener hook = new HookListener();
+                                        HookListener hook = new HookListener();
                     hook.Start();
+                    
+                    System.Console.WriteLine("Executing post scripts...");
+                    if(
+                        File.Exists(ConfigurationManager.AppSettings["AdditionalCommands"] && 
+                        !string.IsNullOrEmpty(File.Exists(ConfigurationManager.AppSettings["AdditionalCommands"])))
+                    ){
+                        foreach(var line in File.ReadAllLines(ConfigurationManager.AppSettings["AdditionalCommands"])){
+                            System.Diagnostics.ProcessStartInfo procStartInfo;                
+                            procStartInfo = new System.Diagnostics.ProcessStartInfo(line); 
+                            procStartInfo.RedirectStandardOutput = true;
+                            procStartInfo.RedirectStandardError = true;
+                            procStartInfo.UseShellExecute = false;
+
+                            procStartInfo.CreateNoWindow = true;
+
+                            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                            proc.StartInfo = procStartInfo;
+                            proc.Start();
+                            // Get the output into a string
+                            System.Console.WriteLine(proc.StandardOutput.ReadToEnd());
+                        }
+
+                    }
+
+
                 }catch{
 
                 }
